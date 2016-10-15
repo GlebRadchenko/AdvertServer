b4fee9bce346a125ff1427307e744f5882adf72f
Gleb Radchenko <glebradchenko@MacBook-Air-Gleb.local>
2016-10-15 21:58:55 +0300

    updated packege file

--- AdvertServer/Package.swift
+++ AdvertServer/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0),
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/vapor/mongo-provider.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/kylef/JSONWebToken.swift.git", majorVersion: 1)
+       // .Package(url: "https://github.com/kylef/JSONWebToken.swift.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
