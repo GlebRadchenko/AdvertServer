567cc2805d9dc54d3017255357fb9d80673a343f
Gleb Radchenko <glebradchenko@MacBook-Air-Gleb.local>
2016-10-15 21:43:38 +0300

    moved all handlers to methods

--- AdvertServer/Sources/App/Controllers/UserController.swift
+++ AdvertServer/Sources/App/Controllers/UserController.swift
@@ -11,6 +11,7 @@ import Vapor
 import Auth
 import Cookies
 import BCrypt
+import HTTP
 
 class UserController {
     weak var drop: Droplet?
@@ -47,58 +48,59 @@ class UserController {
             debugPrint("Drop is nil")
             return
         }
-        drop.group("users") { users in
-            users.post { req in
-                //register
-                guard let name = req.data["name"]?.string,
-                let login = req.data["login"]?.string,
-                let password = req.data["password"]?.string else {
-                    throw Abort.badRequest
-                }
-                if let _ = try User.query().filter("login", login).first() {
-                    throw Abort.custom(status: .conflict, message: "Such user already exist")
-                }
-                var user = User(name: name, login: login, password: password)
-                user.token = self.token(for: user)
-                try user.save()
-                return try user.makeJSON()
-            }
-            users.post("login") { req in
-                guard let login = req.data["login"]?.string,
-                let password = req.data["password"]?.string else {
-                    throw Abort.badRequest
-                }
-                let creds = APIKey(id: login,
-                                   secret: password)
-                try req.auth.login(creds)
-                guard let id = try req.auth.user().id,
-                    let user = try User.find(id) else {
-                    print("user not found")
-                    throw Abort.badRequest
-                }
-                var newUser = User(user: user)
-                newUser.token = self.token(for: user)
-                try user.delete()
-                try newUser.save()
-                let node = ["message": "Logged in", "access_token" : newUser.token]
-                return try JSON(node: node)
-            }
-            users.post("logout", handler: { (req) in
-                guard let token = req.auth.header?.bearer else {
-                    throw Abort.notFound
-                }
-                if let user = try User.query().filter("access_token", token.string).first() {
-                    var newUser = User(user: user)
-                    newUser.token = ""
-                    try newUser.save()
-                    try req.auth.logout()
-                    return try JSON(node: ["error": false,
-                                           "message": "Logout successed"])
-                }
-                throw Abort.badRequest
-            })
-        }
+        let userGroup = drop.grouped("users")
+        userGroup.post(handler: register)
+        userGroup.post("login", handler: login)
+        userGroup.post("logout", handler: logout)
     }
+    func register(_ req: Request) throws -> ResponseRepresentable {
+        guard let name = req.data["name"]?.string,
+            let login = req.data["login"]?.string,
+            let password = req.data["password"]?.string else {
+                throw Abort.badRequest
+        }
+        if let _ = try User.query().filter("login", login).first() {
+            throw Abort.custom(status: .conflict, message: "Such user already exist")
+        }
+        var user = User(name: name, login: login, password: password)
+        user.token = self.token(for: user)
+        try user.save()
+        return try user.makeJSON()
+    }
+    func login(_ req: Request) throws -> ResponseRepresentable {
+        guard let login = req.data["login"]?.string,
+            let password = req.data["password"]?.string else {
+                throw Abort.badRequest
+        }
+        let creds = APIKey(id: login,
+                           secret: password)
+        try req.auth.login(creds)
+        guard let id = try req.auth.user().id,
+            let user = try User.find(id) else {
+                throw Abort.badRequest
+        }
+        var newUser = User(user: user)
+        newUser.token = self.token(for: user)
+        try user.delete()
+        try newUser.save()
+        let node = ["message": "Logged in", "access_token" : newUser.token]
+        return try JSON(node: node)
+    }
+    func logout(_ req: Request) throws -> ResponseRepresentable {
+        guard let token = req.auth.header?.bearer else {
+            throw Abort.notFound
+        }
+        if let user = try User.query().filter("access_token", token.string).first() {
+            var newUser = User(user: user)
+            newUser.token = ""
+            try newUser.save()
+            try req.auth.logout()
+            return try JSON(node: ["error": false,
+                                   "message": "Logout successed"])
+        }
+        throw Abort.badRequest
+    }
+    
     func token(for user: User) -> String {
         return encode(["hash":user.hash], algorithm: .hs256("secret".data(using: .utf8)!))
     }
